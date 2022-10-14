65148e30200fb4e5437065903b503076fa019949
Keith Wagner <244694+kpwags@users.noreply.github.com>
2022-10-13 20:27:46 -0400

    Created a tool to generate social images for blog posts

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SocialImageGenerator.Models;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace SocialImageGenerator;
+
+internal class Program
+{
+    static PostData? data;
+
+    static async Task Main(string[] args)
+    {
+        data = GetPostData();
+
+        if (data is null)
+        {
+            Console.WriteLine("Unable to generate post data");
+            return;
+        }
+
+        await BuildImage();
+    }
+
+    static DateTime GetPostDate(string enteredDate)
+    {
+        DateTime postDate;
+
+        if (!DateTime.TryParse(enteredDate, out postDate))
+        {
+            postDate = DateTime.Now;
+        }
+
+        return postDate;
+    }
+
+    static PostData GetPostData()
+    {
+        Console.Write($"Enter the post's date (defaults to {DateTime.Now.ToString("yyyy-MM-dd")}): ");
+
+        var date = Console.ReadLine();
+
+        var postDate = GetPostDate(date ?? "");
+
+        Console.Write($"Enter the post's Title: ");
+
+        var postTitle = Console.ReadLine();
+
+        Console.Write($"Enter the post's slug: ");
+
+        var postSlug = Console.ReadLine();
+
+        return new PostData
+        {
+            Title = postTitle ?? "",
+            Slug = postSlug ?? "",
+            PostDate = postDate,
+        };
+    }
+
+    static async Task BuildImage()
+    {
+        if (data is null)
+        {
+            throw new Exception("Post Data is null");
+        }
+
+        using var image = Image.Load("Template.jpg");
+
+        var petrona = new FontCollection();
+
+        petrona.AddSystemFonts();
+
+        if (petrona.TryGet("Helvetica Neue", out FontFamily family))
+        {
+            var font = family.CreateFont(60, FontStyle.Bold);
+
+            var options = new TextOptions(font)
+            {
+                Origin = new PointF(40, 60),
+                WrappingLength = 600,
+                HorizontalAlignment = HorizontalAlignment.Left,
+            };
+
+            var rect = TextMeasurer.Measure(data.Title, options);
+
+            // image.Mutate(x => x.DrawText(data.Title, font, Color.White, new PointF(40, 80)));
+            image.Mutate(x => x.DrawText(options, data.Title, Color.White));
+
+            var urlFont = family.CreateFont(24, FontStyle.Regular);
+
+            var urlOptions = new TextOptions(font)
+            {
+                Origin = new PointF(40, 80 + rect.Height),
+                WrappingLength = 600,
+                HorizontalAlignment = HorizontalAlignment.Left,
+            };
+
+            image.Mutate(x => x.DrawText(data.Url, urlFont, Color.White, new PointF(40, 80 + rect.Height)));
+        }
+
+        using var ms = new MemoryStream();
+
+        await image.SaveAsync(ms, new JpegEncoder());
+
+        var imageData = ms.ToArray();
+
+        var decoder = new JpegDecoder();
+
+        await image.SaveAsJpegAsync(BuildFilePath());
+    }
+
+    static string BuildFilePath()
+    {
+        if (data is null)
+        {
+            throw new Exception("Post Data is null");
+        }
+
+        string rootDirectory = string.Empty;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            rootDirectory = "/Users/keith/Developer/kpwags.com/public/images/posts";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            rootDirectory = @"C:\Users\keith\Developer\kpwags.com\public\images\posts";
+        }
+        else
+        {
+            throw new Exception("Invalid Operating System");
+        }
+
+        var directory = Path.Join(rootDirectory, data.Directory);
+
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        return Path.Join(directory, "social-image.jpg");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# social-image-generator
-A .NET Console App to generate social images for my blog posts
+# Social Image Generator
+
+I built this little console app to generate social images for my blog posts. Faster and easier than opening Photoshop. It uses the [Six Labors ImageSharp](https://docs.sixlabors.com/index.html) library to write text on my template image.
+
+It takes some input from the console and outputs the image to the appropriate directory. I might end up trying to automate more of it as time goes on, but for now it takes the date, slug & title and it does the rest automatically.
+
+## Template
+
+![Template Image](Template.jpg "Template Image")
+
+## Example Output
+
+![Example Output Image](ExampleOutput.jpg "Example Output Image")
