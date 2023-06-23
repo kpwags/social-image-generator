bed3e109737b15d198e234964a09a45dcf5c6ca2
Keith Wagner <244694+kpwags@users.noreply.github.com>
2023-06-23 17:19:58 -0400

    Moved the configuration to appsettings.json
    Moved the directory configuration to the `appsettings.json` file.
    
    Closes #18

---
+++ Models/DirectorySettings.cs
@@ -0,0 +1,12 @@
+namespace SocialImageGenerator.Models;
+
+public class DirectorySettings
+{
+    public string Mac { get; set; } = string.Empty;
+    
+    public string Windows { get; set; } = string.Empty;
+    
+    public string Posts { get; set; } = string.Empty;
+    
+    public string ReadingLogs { get; set; } = string.Empty;
+}
